333fd988eca27117c82deebc50621e1663d65b4d
MMike17 <matthews.r.mike@gmail.com>
2026-02-20 12:27:24 +0100

    Hide original panels

--- Patches.cs
+++ Patches.cs
@@ -48,46 +48,24 @@ namespace HistoricalCareer
 
             if (panel.name.Contains(GROUP_PANEL_FORMAT))
             {
-                HorizontalLayoutGroup group = panel.GetComponentInChildren<HorizontalLayoutGroup>();
-                group.spacing = -10;
-                group.gameObject.AddComponent<ContentSizeFitter>().horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+                HorizontalLayoutGroup layout = panel.GetComponentInChildren<HorizontalLayoutGroup>();
+                layout.spacing = -10;
+                layout.gameObject.AddComponent<ContentSizeFitter>().horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
 
-                CarrouselUI carrousel = group.GetComponent<CarrouselUI>();
+                // hide buttons
+                foreach (Transform child in layout.transform)
+                    child.gameObject.SetActive(false);
+
+                // TODO : Spawn new panels with new pictures and text (set season)
+                // TODO : Detect which group we are in
+
+                // add custom UI
+                CarrouselUI carrousel = layout.GetComponent<CarrouselUI>();
 
                 if (carrousel == null)
-                    carrousel = group.gameObject.AddComponent<CarrouselUI>();
+                    carrousel = layout.gameObject.AddComponent<CarrouselUI>();
 
                 carrousel.Reset();
-
-                // TODO : Destroy / hide the og panels
-                // TODO : Spawn new panels with new pictures and text
-                // TODO : Detect which group we are in
-                // TODO : Rework data structure to have easy access by group (Season needs to be part of rally settings)
-                // TODO : How do I do selection ?
-
-                //while (group.transform.childCount != 7)
-                //{
-                //    Selectable previous = group.transform.GetChild(group.transform.childCount - 1).GetComponent<Selectable>();
-                //    Transform newItem = GameObject.Instantiate(group.transform.GetChild(0), group.transform);
-
-                //    newItem.GetComponent<CustomButtonSeason>().Init();
-                //    newItem.GetComponent<CustomButtonSeason>().SetText(new Season(1990, Car.CarClass.GROUP_2, 1, 2, 1, string.Empty, false, AIDriverSkillTables.AI_Skill.EASY));
-
-                //    Navigation nav = previous.navigation;
-                //    nav.selectOnRight = newItem.GetComponent<Selectable>();
-                //    previous.navigation = nav;
-
-                //    newItem.GetComponent<Selectable>().navigation = new Navigation() { selectOnLeft = previous };
-                //}
-
-                //foreach (Transform item in group.transform)
-                //{
-                //    PropertyInfo info = typeof(Selectable).GetProperty("hasSelection", BindingFlags.NonPublic | BindingFlags.Instance);
-                //    bool isSelected = (bool)info.GetValue(item.GetComponent<Selectable>());
-                //    Main.Log(item.name + " selected : " + isSelected);
-                //}
-
-                //group.transform.GetChild(0).GetComponent<CustomButtonSeason>().onClick.RemoveAllListeners();
             }
 
             switch (panel.name)
@@ -99,11 +77,10 @@ namespace HistoricalCareer
                 case CAR_PANEL:
                     if (inCareer)
                     {
-                        // TODO : No need to detect by year anymore => store on selection
-
-                        int year = GameModeManager.CareerManager.GetCurrentSeason().Year;
-                        Main.Log("Starting season " + year);
-                        RallyManager.AppyRallySettings(year);
+                        // TODO : No need to detect by year anymore
+                        //int year = GameModeManager.CareerManager.GetCurrentSeason().Year;
+                        //Main.Log("Starting season " + year);
+                        //RallyManager.AppyRallySettings(year);
 
                         // TODO : Show rally and driver details above car selection (disable OG UI)
                         //panel.StartCoroutine(WaitAndActivate(0.01f, () => panel.GetComponent<CarChooserHelper>().BeginEvent()));
@@ -122,9 +99,6 @@ namespace HistoricalCareer
         }
 
         [HarmonyPatch(nameof(PanelManager.GoBack))]
-        static void Postfix()
-        {
-            inCareer = false;
-        }
+        static void Postfix() => inCareer = false;
     }
 }
