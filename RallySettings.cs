15ea2ac7e3676d264be0d0fe666ef3b471efb8bf
MMike17 <matthews.r.mike@gmail.com>
2026-02-20 11:57:26 +0100

    Rework settings

--- RallySettings.cs
+++ RallySettings.cs
@@ -4,38 +4,77 @@ namespace HistoricalCareer
 {
     public class RallySettings
     {
-        // TODO : Restructure to be able to have multiple rallies in a season
-        // should this class be called season settings ?
-
-        public Car.CarClass carClass;
+        public Car.CarClass carClass; // TODO : do we really need this ?
         public Car car;
         public Livery livery;
-        public RallyData rallyData;
+        public Season season;
 
         /// <param name="car">Use CarManager.GetCurrentCarsListForClass to get cars</param>
         /// <param name="livery">Use RallySettings.GetCarLiveries (with car.prefabName) to get car liveries</param>
+        /// <param name="stagesIndeces">No double stages, stages go by pairs (0-1, 2-3, etc...)</param>
         /// <param name="weathers">Use AreaManager.GetWeatherForCurrentArea to get valid weathers</param>
-        public RallySettings(Car car, Livery livery, AreaManager.Areas area, int[] stagesIndeces, ConditionTypes.Weather[] weathers)
+        public RallySettings(int year, Car car, Livery livery, AreaManager.Areas area, int[] stagesIndeces, ConditionTypes.Weather[] weathers)
         {
             carClass = car.carClass;
             this.car = car;
             this.livery = livery;
 
-            // TODO : What are the rally making rules ?
-            // No double stage (same stage, same direction) in a rally
-            // check if stages indeces are in bounds (stages list is static)
-            // check if weathers are in bounds (weather list is static)
+            // check stages
+            List<int> correctedStages = new List<int>();
+            int maxIndex = AreaManager.AreaDictionary[area].stageList.Count;
 
-            rallyData = new RallyData();
-            rallyData.SetArea((int)area);
-            rallyData.SetStageCount(stagesIndeces.Length);
+            foreach (int index in stagesIndeces)
+            {
+                if (index >= maxIndex)
+                    Main.Error("Stage index " + index + " is out of bounds (" + maxIndex + "), the stage will be ignored.");
+                else
+                    correctedStages.Add(index);
+            }
+
+            stagesIndeces = correctedStages.ToArray();
+
+            // check weathers
+            List<ConditionTypes.Weather> areaWeathers = AreaManager.GetWeatherForCurrentArea(area);
+            List<ConditionTypes.Weather> correctedWeathers = new List<ConditionTypes.Weather>();
+
+            foreach (ConditionTypes.Weather weather in weathers)
+            {
+                if (!areaWeathers.Contains(weather))
+                    Main.Error("Weather " + weather + " is invalid for this area (" + area + "), the weather will be ignored.");
+                else
+                    correctedWeathers.Add(weather);
+            }
+
+            weathers = correctedWeathers.ToArray();
+
+            // setup season
+            int restarts = 0; // TODO : What's the OG number of restarts for that season ? Do I need to change that ?
+            string bonusSaveConstant = null; // TODO : What the hell is supposed to be the BonusSaveConstant ?
+            Season.STATUS status = Season.STATUS.UNLOCKED; // TODO : Manage season state from the save system
+
+            season = new Season(
+                year,
+                carClass,
+                1, // TODO : Not sure if I need to be able to go above 1 rally
+                stagesIndeces.Length,
+                restarts,
+                bonusSaveConstant,
+                true,
+                AIDriverSkillTables.AI_Skill.EASY,
+                status
+            );
+
+            // setup rally
+            season.Rallies[0] = new RallyData();
+            season.Rallies[0].SetArea((int)area);
+            season.Rallies[0].SetStageCount(stagesIndeces.Length);
 
             for (int i = 0; i < stagesIndeces.Length; i++)
             {
                 int index = stagesIndeces[i];
                 Stage stage = AreaManager.GetStageByIndex(ref index, area);
-                rallyData.SetStage(i, stage);
-                rallyData.SetWeatherForStage(i, weathers[i]);
+                season.Rallies[0].SetStage(i, stage);
+                season.Rallies[0].SetWeatherForStage(i, weathers[i]);
             }
         }
 
