a04184e39a9c1a892ec830be507d5c8a7b3f151c
MMike17 <matthews.r.mike@gmail.com>
2026-02-23 18:02:40 +0100

    Add career UI classes

---
+++ CareerUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HistoricalCareer
+{
+    internal class CareerUI : MonoBehaviour
+    {
+        private Text seasonDate;
+        private Text rallyName;
+        private Polaroid environmentPolaroid;
+        private Polaroid pilotPolaroid;
+        private Image carPicture;
+        private Text contextText;
+
+        // TODO : Add StyleImageColour to polaroids when spawning window
+        // off color = #FFFFFFFF
+        // on color = #D3D3D3FF
+
+        public void Set(RallySettings settings)
+        {
+            if (seasonDate == null)
+            {
+                Transform titleHolder = transform.GetChild(0);
+                seasonDate = titleHolder.GetChild(0).GetComponent<Text>();
+                rallyName = titleHolder.GetChild(1).GetComponent<Text>();
+
+                Transform displayHolder = transform.GetChild(1);
+                environmentPolaroid = displayHolder.GetChild(0).gameObject.AddComponent<Polaroid>();
+                pilotPolaroid = displayHolder.GetChild(1).gameObject.AddComponent<Polaroid>();
+                carPicture = displayHolder.GetChild(2).GetComponent<Image>();
+
+                contextText = transform.GetChild(2).GetComponent<Text>();
+            }
+
+            seasonDate.text = settings.season.Year.ToString();
+            rallyName.text = settings.rallyName;
+            environmentPolaroid.SetPicture(null, settings.season.Rallies[0].CurrentArea.ToString()); // TODO : How do I source the country pictures ?
+            pilotPolaroid.SetPicture(settings.pilotPicture, settings.pilotName + "(" + settings.pilotPictureYear + ")");
+            //carPicture.sprite = ; // TODO : How do I get the car picture ?
+            contextText.text = settings.loreText;
+        }
+    }
+}
---
+++ Polaroid.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using static UnityEngine.RectTransform;
+
+namespace HistoricalCareer
+{
+    internal class Polaroid : MonoBehaviour
+    {
+        const float PICTURE_RATIO = 2.4f / 2.8f;
+        const float TEXT_RATIO = 0.4f / 3.4f;
+
+        private RectTransform polaroid;
+        private RectTransform pictureFrame;
+        private Image picture;
+        private Text caption;
+
+        public void SetPicture(Sprite picture, string text)
+        {
+            if (polaroid == null)
+            {
+                polaroid = GetComponent<RectTransform>();
+                pictureFrame = transform.GetChild(0).GetComponent<RectTransform>();
+                this.picture = pictureFrame.GetChild(0).GetComponent<Image>();
+                caption = transform.GetComponentInChildren<Text>();
+            }
+
+            float polaroidWidth = polaroid.rect.width;
+            float polaroidHeight = polaroid.rect.height;
+            float frameWidth = polaroidWidth * PICTURE_RATIO;
+            float frameSpacing = (polaroidWidth - frameWidth) / 2;
+            pictureFrame.SetSizeWithCurrentAnchors(Axis.Horizontal, frameWidth);
+            pictureFrame.SetSizeWithCurrentAnchors(Axis.Vertical, frameWidth);
+            pictureFrame.localPosition = new Vector3(0, polaroidHeight / 2 - frameWidth / 2 - frameSpacing, 0);
+
+            float textHeight = TEXT_RATIO * polaroidHeight;
+            caption.rectTransform.SetSizeWithCurrentAnchors(Axis.Horizontal, frameWidth * 0.9f);
+            caption.rectTransform.SetSizeWithCurrentAnchors(Axis.Vertical, textHeight);
+            caption.transform.localPosition = new Vector3(0, -polaroidHeight / 2 + textHeight / 2 + frameSpacing, 0);
+
+            this.picture.sprite = picture;
+            caption.text = text;
+        }
+    }
+}
